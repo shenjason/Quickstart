6d8dd7b1d8e7fbf19e159e2eb9d564d8b256a394
qwertyuio606 <shenandy69@gmail.com>
2025-11-25 16:28:52 -0800

    Update Turret.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/assemblies/Turret.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/assemblies/Turret.java
@@ -13,15 +13,24 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
 import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 import org.firstinspires.ftc.teamcode.util.Assembly;
-
+import org.firstinspires.ftc.teamcode.util.PIDcontroller;
+import org.opencv.core.Mat;
 
 
 public class Turret extends Assembly {
+    public double P =1,I=0,D=0;
+    public PIDcontroller turretController;
+    private double targetRotation;
+    public double Ta;
+    private double Tx;
     private Limelight3A limelight;
-    private IMU imu;
 
     private DcMotor turretMotor;
     private Follower follower;
+
+    private double targetPointX;
+
+    private static double  TARGETBLUEX=11, TARGETY=137.1, TARGETREDX=144-TARGETBLUEX;
 
     public final static int GPP = 0, PGP = 1, PPG = 2, BLUE_TARGET_LINE = 3, RED_TARGET_LINE = 4, TRACKING_MODE = 0, IDLE_MODE = 1;
     public int mode = IDLE_MODE;
@@ -40,41 +49,40 @@ public class Turret extends Assembly {
         limelight.setPollRateHz(30);
 
         limelight.pipelineSwitch((side) ? BLUE_TARGET_LINE : RED_TARGET_LINE);
+        targetPointX = (side) ? TARGETBLUEX : TARGETREDX;
+
+        turretController = new PIDcontroller(P, I, D, -1, 1);
+
     }
 
 
     @Override
     public void update() {
-        YawPitchRollAngles orientation = imu.getRobotYawPitchRollAngles();
-        limelight.updateRobotOrientation(orientation.getYaw());
 
         LLResult llResult = limelight.getLatestResult();
         Pose cp = follower.getPose();
-        double ANGLE = getAngle(cp.getX(),cp.getY(),72.0,0.0);
+        double angle = getAngle(cp.getX(),cp.getY(),targetPointX,TARGETY);
+        Ta = 0;
+        Tx = 0;
+        double robotAngle = cp.getHeading();
         if (limelightResultVaild(llResult)) {
-            //Pose3D botPose = llResult.getBotpose();
-
-            debugAddData("Tx", llResult.getTx());
-            debugAddData("Ty", llResult.getTy());
-            debugAddData("Ta", llResult.getTa());
-//            if (Math.abs(llResult.getTx())<5.0)
-//            {
-//                turretMotor.setPower(Math.signum(ANGLE));
-//            }
+            Ta = llResult.getTx();
+            Tx = llResult.getTx();
+            debugAddData("Ta", Ta);
+            debugAddData("Tx", Tx);
         }
-//        else{
-//            if (ANGLE<0.0){
-//                turretMotor.setPower(0.1);
-//            }
-//            else{
-//                turretMotor.setPower(-0.1);
-//            }
-//        }
+        double rawDiff = angle - robotAngle;
+        double diffAngle = rawDiff-2*Math.PI*((rawDiff>Math.PI)?1:-1);
+        targetRotation = diffAngle - Tx;
+        double currentRotation = turretMotor.getCurrentPosition()/145.1*16/100*2*Math.PI;
+        debugAddData("currentRotation", currentRotation);
+        turretMotor.setPower(turretController.step((Math.abs(targetRotation)<=Math.PI/2)?targetRotation:Math.PI/2*Math.signum(targetRotation) , currentRotation));
+
 
         debugAddData("PoseX", cp.getX());
         debugAddData("PoseY", cp.getY());
         debugAddData("Heading", Math.toDegrees(cp.getHeading()));
-        debugAddData("Angle bot->tag approx", ANGLE);
+        debugAddData("Angle bot->tag approx",diffAngle);
 
     }
 
@@ -103,14 +111,6 @@ public class Turret extends Assembly {
     }
 
     public static double getAngle(double a, double b, double a2, double b2) {
-        double sign = Math.signum(a2 - a);
-
-        double atanRad = Math.atan2(b2 - b,a2 - a);
-
-        double term1 = Math.toRadians(90) - atanRad;
-        double term2 = -Math.toRadians(180) * ((1 - sign) / 2.0);
-        double term3 = Math.toRadians(90) * (1 - Math.abs(sign));
-
-        return term1 + term2 + term3;
+        return Math.atan2(a2-a,b2-b);
     }
 }
